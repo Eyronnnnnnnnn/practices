e1538ecf39f52f1a3df9084be4e312c5f21c6ce7
EYRON <aaronguillermo123456789@gmail.com>
2026-08-27 16:31:15 +0800

    updated

--- QuizAppFinalProject/Ui.js
+++ QuizAppFinalProject/Ui.js
@@ -1,15 +1,16 @@
- 
- 
- export function setupSidebarNav() {
-    const navLinks = document.querySelectorAll("#sidebar-nav a");
-    navLinks.forEach((navs) => {
-      navs.addEventListener("click", (event) => {
-        event.preventDefault();
-      //  handleNavClick(navs);
-      
-        console.log("hellowoorld");
+  
+  
+  export function setupSidebarNav() {
+      const navLinks = document.querySelectorAll("#sidebar-nav a");
+      navLinks.forEach((navs) => {
+        navs.addEventListener("click", (event) => {
+          event.preventDefault();
+          
+        
+        
+          console.log("hellowoorld");
+        });
       });
-    });
-  }
+    }
 
 
