8bae9607be09375656eb511c271e061a3f4ab8e2
EYRON <aaronguillermo123456789@gmail.com>
2026-08-28 21:39:34 +0800

    day 40

--- practice-mini-projects/QuizAppFinalProjectPhase1/Quiz_logic.js
+++ practice-mini-projects/QuizAppFinalProjectPhase1/Quiz_logic.js
@@ -1,7 +1,6 @@
 let currentQuestionIndex = 0;
 let Quiz_Score = 0;
 
-
 const Quiz_Questions = [
   {
     id: 1,
@@ -32,80 +31,72 @@ const Quiz_Questions = [
 ];
 
 let questionlenght = Quiz_Questions.length;
-  const getQuestion = document.querySelector(".question");
-  const getButtons = document.querySelectorAll(".options button");
-  const getScore = document.getElementById("score");
-  const getResetBtn = document.querySelectorAll("resetbtn");
+const getQuestion = document.querySelector(".question");
+const getButtons = document.querySelectorAll(".options button");
+const getScore = document.getElementById("score");
+const getResetBtn = document.querySelector("#resetbtn");
 
 export function displayQuestion() {
-    
- 
-//   console.log(Quiz_Questions);
- getQuestion.textContent = Quiz_Questions[currentQuestionIndex].Question;
- getButtons.forEach((btn , index)=>{
-     btn.innerHTML = Quiz_Questions[currentQuestionIndex].Options[index];
-     btn.addEventListener("click",(event)=>{
-        if(event.target.textContent === Quiz_Questions[currentQuestionIndex].Answer){
-            console.log("correct");
-            Quiz_Score++
-            btn.style.backgroundColor = "#00a608";
+  //   console.log(Quiz_Questions);
+  getQuestion.textContent = Quiz_Questions[currentQuestionIndex].Question;
+  getButtons.forEach((btn, index) => {
+    btn.innerHTML = Quiz_Questions[currentQuestionIndex].Options[index];
+    btn.addEventListener("click", (event) => {
+      if (
+        event.target.textContent === Quiz_Questions[currentQuestionIndex].Answer
+      ) {
+        console.log("correct");
+        Quiz_Score++;
+        btn.style.backgroundColor = "#00a608";
         getScore.textContent = `Score: ${Quiz_Score}`;
-        setTimeout(()=>{
-            UpdateQuestion();
-         
-        },2000)
-        }else{
-             btn.style.backgroundColor = "red";
+        setTimeout(() => {
+          UpdateQuestion();
+        }, 2000);
+      } else {
+        btn.style.backgroundColor = "red";
         console.log("Wrong Answer!!");
         btn.classList.add("shake");
-
-
 
         setTimeout(() => {
           btn.style.backgroundColor = "#1e3c72";
         }, 300);
-        }
-     })
- })
+      }
+    });
+  });
 
-  getResetBtn.addEventListener("click", ()=>{
+  getResetBtn.addEventListener("click", () => {
     resetQuiz();
     console.log("hello");
   });
-
-
 }
 
-function UpdateQuestion(){
-    console.log(currentQuestionIndex);
-    currentQuestionIndex++
-    if(currentQuestionIndex >= questionlenght ){
-      console.log("your quiz is finish");
-       getButtons.forEach((btn,index)=>{
-        btn.disabled = true ;
-       })
-    }else{
-      getQuestion.textContent =  Quiz_Questions[currentQuestionIndex].Question  ;
-      getButtons.forEach((btn,index)=>{
-        btn.style.backgroundColor = "#1e3c72";
-        btn.innerHTML = Quiz_Questions[currentQuestionIndex].Options[index];
-      })
-    }
-
-
-
+function UpdateQuestion() {
+  console.log(currentQuestionIndex);
+  currentQuestionIndex++;
+  if (currentQuestionIndex >= questionlenght) {
+    console.log("your quiz is finish");
+    getButtons.forEach((btn, index) => {
+      btn.disabled = true;
+    });
+  } else {
+    getQuestion.textContent = Quiz_Questions[currentQuestionIndex].Question;
+    getButtons.forEach((btn, index) => {
+      btn.style.backgroundColor = "#1e3c72";
+      btn.innerHTML = Quiz_Questions[currentQuestionIndex].Options[index];
+    });
+  }
 }
 
-function resetQuiz(){
+function resetQuiz() {
   currentQuestionIndex = 0;
-  Quiz_Score = 0 ;
+  Quiz_Score = 0;
 
-  getButtons.forEach((btn,index)=>{
+  getButtons.forEach((btn, index) => {
     btn.innerHTML = Quiz_Questions[currentQuestionIndex].Options[index];
-  })
+    btn.disabled = false;
+     btn.style.backgroundColor = "#1e3c72";
+  });
 
-  getQuestion.forEach((question)=>{
-    question.innerHTML ="hello"
-  })
-
+  getQuestion.innerHTML = Quiz_Questions[currentQuestionIndex].Question;
+  getScore.textContent = `Score: ${Quiz_Score}`;
 }
